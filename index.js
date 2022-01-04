9c88f3013972b3a5674e583e68b7b38ca89f2683
DrDano <danielharned@gmail.com>
2022-01-03 19:54:38 -0800

    adding instructions for project creation as comments in index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// prompt user for name, github username, and email, then create links to github profile and email address in readme
+// prompt user for a project title and validate response
+// prompt user for project description and validate response
+// prompt user for installation instructions in a step-wise fashion (maybe even ask them for one step at a time and validate after each one to see if they are done adding steps) and validate response
+// prompt user for usage instructions and validate response
+// prompt user for credits and validate response
+// give user multiple choices for license
+// give user multiple choices for badges
+// prompt user for optional contribution instructions
+// prompt user for any optional tests
+
+// create table of contents which is dependent on what optional responses the user filled out
+
+// code generateMarkdown.js to create the markdown file from user responses
+// create promise chain in index.js to create MD file from generateMarkdown.js
+
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
